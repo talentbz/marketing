fb4a877a3e62937c5cf4832248dbe0924a3f8521
rockstar <leragnezdina@mail.ru>
2022-11-16 04:41:41 +0300

    as

--- app/Http/Controllers/HyrosApiController.php
+++ app/Http/Controllers/HyrosApiController.php
@@ -9,19 +9,6 @@ class HyrosApiController extends Controller
 {
     public function getMTD( Request $request){
         // return view('welcome');
-        // $request = Http::get('https://private-ea0372-hyros.apiary-mock.com/v1/api/v1.0/attribution', [
-        //     "attributionModel" => 'last_click',
-        //     "startDate" => '2020-05-12T10:00:00',
-        //     "endDate" => '2021-04-13T10:00:00',
-        //     "level" => 'level',
-        //     "fields" => 'fields',
-        //     "ids" => '205044496234,205044496235',
-        //     "dayOfAttribution" => false,
-        //     "scientificDaysRange" => 30
-        // ]);
-        // $request->headers->set('Content-Type', 'application/json');
-        // $request->headers->set('API-Key', 'b12a19f4521d44abc8d613efca7f9c23c88');
-        // dd($request);
         $response = Http::withHeaders([
             'Content-Type' => 'application/json',
             'API-Key' => 'b12a19f4521d44abc8d613efca7f9c23c88', 
@@ -35,7 +22,7 @@ class HyrosApiController extends Controller
             "dayOfAttribution" => false,
             "scientificDaysRange" => 30
         ]);
-        $data = $response->getBody()->getContents();
+        $data = $response->getBody()->getContents()->result;
         dd($data);
         return json_decode($response);
     }
