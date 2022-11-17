cf258ec29caecfc66723d531fd3e1a19b0944813
rockstar <leragnezdina@mail.ru>
2022-11-17 16:14:51 +0300

    asd

--- app/Http/Controllers/HyrosApiController.php
+++ app/Http/Controllers/HyrosApiController.php
@@ -73,7 +73,7 @@ class HyrosApiController extends Controller
             'currency' => 'user_currency',
             "level" => 'google_campaign',
             "fields" => 'revenue, sales',
-            "ids" => '13021951885, 13048019932, 13066429405, 13530231803, 18828997569, 16913767610',
+            "ids" => '13048019932, 13066429405, 13530231803, 18828997569, 16913767610',
             "dayOfAttribution" => false,
         ]);
         $data = json_decode($response->getBody()->getContents());
