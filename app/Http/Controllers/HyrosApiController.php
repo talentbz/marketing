0a4eee17bff2f2bb7a24f79283261473527a8542
rockstar <leragnezdina@mail.ru>
2022-11-17 09:53:16 +0300

    asd

--- app/Http/Controllers/HyrosApiController.php
+++ app/Http/Controllers/HyrosApiController.php
@@ -46,7 +46,7 @@ class HyrosApiController extends Controller
             "endDate" => '2022-11-15',
             "level" => 'google_ad',
             "fields" => 'revenue,total_revenue,refund,unique_sales',
-            "ids" => 'ids=6896178013',
+            "ids" => '6896178013',
             "dayOfAttribution" => false,
             "scientificDaysRange" => 30
         ]);
