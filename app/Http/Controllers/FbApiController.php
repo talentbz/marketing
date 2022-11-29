d2ddb0758bbee5bf4f848f08031f0be8d8ffb9c2
rockstar <leragnezdina@mail.ru>
2022-11-29 20:09:00 +0300

    asd

--- app/Http/Controllers/FbApiController.php
+++ app/Http/Controllers/FbApiController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use FacebookAds\Object\AdAccount;
 use FacebookAds\Object\Fields\AdAccountFields;
+use FacebookAds\Object\Fields\InsightsFields;
 use FacebookAds\Object\Campaign;
 use FacebookAds\Api;
 use FacebookAds\Logger\CurlLogger;
@@ -25,7 +26,24 @@ class FbApiController extends Controller
         //     AdAccountFields::NAME,
         //   );
           
-          $account = (new AdAccount($id))->getInsights();
+          $account = new AdAccount($id);
+          $fields = array(
+            InsightsFields::AD_NAME,
+            InsightsFields::ADSET_NAME,
+            InsightsFields::CAMPAIGN_NAME,
+            InsightsFields::ACCOUNT_NAME,
+            InsightsFields::ACCOUNT_ID,
+            InsightsFields::IMPRESSIONS,
+            InsightsFields::INLINE_LINK_CLICKS,
+            InsightsFields::SPEND,
+            InsightsFields::AD_ID
+        );
+        
+        $params = array(
+            'date_preset' => InsightsPresets::YESTERDAY
+        );
+        
+        $account->getInsights($fields, $params);
           dd($account);
         // $facebookAds = LaravelAds::facebookAds()->with(806688472849920);
         // $campaigns = $facebookAds->fetch()->getCampaigns();
