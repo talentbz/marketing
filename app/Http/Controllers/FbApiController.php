54d9a423f19cad5c0da9f05f2344322d383fd65e
rockstar <leragnezdina@mail.ru>
2022-11-29 21:57:02 +0300

    asd

--- app/Http/Controllers/FbApiController.php
+++ app/Http/Controllers/FbApiController.php
@@ -19,8 +19,8 @@ class FbApiController extends Controller
         $access_token = env('FB_ACCESS_TOKEN');
         $app_secret = ('FB_APP_SECRET');
         $app_id = env('FB_APP_ID');
-        // $id = env('FB_ACCOUNT_ID');
-        $id= 'act_806688472849920';
+        $id = env('FB_ACCOUNT_ID');
+        // $id= 'act_806688472849920';
         $api = Api::init($app_id, $app_secret, $access_token);
         $api->setLogger(new CurlLogger());
         $fields = array(
