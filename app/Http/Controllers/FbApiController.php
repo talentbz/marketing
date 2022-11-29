4fd252e280301fcf1a413a15b7330db28dbea19a
rockstar <leragnezdina@mail.ru>
2022-11-29 21:33:18 +0300

    asd

--- app/Http/Controllers/FbApiController.php
+++ app/Http/Controllers/FbApiController.php
@@ -16,16 +16,16 @@ use FacebookAds\Object\Values\InsightsResultDatePresetValues;
 class FbApiController extends Controller
 {
     public function getMTD( Request $request){
-        // $access_token = env('FB_ACCESS_TOKEN');
-        // $app_secret = ('FB_APP_SECRET');
-        // $app_id = env('FB_APP_ID');
+        $access_token = env('FB_ACCESS_TOKEN');
+        $app_secret = ('FB_APP_SECRET');
+        $app_id = env('FB_APP_ID');
         $id = env('FB_ACCOUNT_ID');
-        // $api = Api::init($app_id, $app_secret, $access_token);
-        // $api->setLogger(new CurlLogger());
-        // $fields = array(
-        //     AdAccountFields::ID,
-        //     AdAccountFields::NAME,
-        //   );
+        $api = Api::init($app_id, $app_secret, $access_token);
+        $api->setLogger(new CurlLogger());
+        $fields = array(
+            AdAccountFields::ID,
+            AdAccountFields::NAME,
+        );
           
           $account = new AdAccount($id);
           dd($account);
@@ -34,7 +34,7 @@ class FbApiController extends Controller
         );
         
         $params = array(
-            // 'date_preset' => InsightsResultDatePresetValues::THIS_MONTH,
+            'date_preset' => InsightsResultDatePresetValues::THIS_MONTH,
         );
         
         $account->getInsights($fields, $params);
