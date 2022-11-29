b05137a70634349161b49f3b419e08b01f616834
rockstar <leragnezdina@mail.ru>
2022-11-29 19:53:13 +0300

    asd

--- app/Http/Controllers/FbApiController.php
+++ app/Http/Controllers/FbApiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use FacebookAds\Object\AdAccount;
+use FacebookAds\Object\Fields\AdAccountFields;
 use FacebookAds\Object\Campaign;
 use FacebookAds\Api;
 use FacebookAds\Logger\CurlLogger;
@@ -19,16 +20,13 @@ class FbApiController extends Controller
         $id = env('FB_ACCOUNT_ID');
         $api = Api::init($app_id, $app_secret, $access_token);
         $api->setLogger(new CurlLogger());
-        dd($api);
         $fields = array(
-        'name',
-        );
-        $params = array(
-        );
-        echo json_encode((new AdAccount($id))->getAdCreatives(
-        $fields,
-        $params
-        )->getResponse()->getContent(), JSON_PRETTY_PRINT);
+            AdAccountFields::ID,
+            AdAccountFields::NAME,
+          );
+          
+          $account = (new AdAccount($id))->getSelf($fields);
+          dd($account);
         // $facebookAds = LaravelAds::facebookAds()->with(806688472849920);
         // $campaigns = $facebookAds->fetch()->getCampaigns();
         // dd($campaigns);
