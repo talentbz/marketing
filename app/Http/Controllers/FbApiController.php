a415e0d4641d903066fd6b79a867754bfb428fd7
rockstar <leragnezdina@mail.ru>
2022-12-20 05:19:25 +0300

    add every month and ytd

--- app/Http/Controllers/FbApiController.php
+++ app/Http/Controllers/FbApiController.php
@@ -11,7 +11,10 @@ use Airtable;
 class FbApiController extends Controller
 {
     public function getMTD( Request $request){
-
+        echo 'First Date    = ' . date('2022-02-01') . '<br />';
+        $first_day = date('2022-02-01');
+        echo 'Last Date     = ' . date('2022-02-t', strtotime($first_day))  . '<br />';
+        dd(1);
         $id_array = [
           // '833160470451699',
           '806691966182904',
@@ -27,8 +30,12 @@ class FbApiController extends Controller
           '563903348250218'
       ];
       $access_token = env('FB_ACCESS_TOKEN');
-      $date_preset = 'this_month';
       $total_account_arr = array();
+      $this_year = date('y');
+      $current_month = date('m');
+      $today = date('d');
+      $month_name_arr = ["nothing", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      
       foreach($id_array as $row){
           $individual_acc_arr = array();
           $individual_acc_arr['FB_Account_ID'] = $row;
@@ -40,7 +47,7 @@ class FbApiController extends Controller
           $individual_acc_arr['Store_Name'] = $account_name;
           
           //get mtd data
-          $mtd_url = 'https://graph.facebook.com/v15.0/act_'.$row.'/insights?date_preset='.$date_preset.'&access_token='.$access_token;
+          $mtd_url = 'https://graph.facebook.com/v15.0/act_'.$row.'/insights?date_preset=this_month&access_token='.$access_token;
           $mtd_response =Http::get($mtd_url);
           $mtd_data = json_decode($mtd_response->getBody()->getContents());
           $mtd = 0;
@@ -49,6 +56,39 @@ class FbApiController extends Controller
           }
           $individual_acc_arr['MTD_Cost'] = (float)$mtd;
 
+          //get every month MTD
+          for ($j = 1; $j <= 12; $j++){
+            $individual_acc_arr[$month_name_arr[$j]] = 0;
+          }
+          for ($i = 1; $i <= $current_month; $i++){
+            if ($i < 10){
+                $month_str = (string)("0".$i);
+            }else{
+                $month_str = (string)$i;
+            }
+            $date_start = $this_year.'-'.$month_str.'-'.'01';
+            $date_stop = date($this_year.'-'.$month_str.'-t', strtotime($first_day));
+            $mtd_url = 'https://graph.facebook.com/v15.0/act_'.$row.'/insights?date_start='.$date_start.'&date_stop='.$date_stop.'&access_token='.$access_token;
+            $mtd_response =Http::get($mtd_url);
+            $mtd_data = json_decode($mtd_response->getBody()->getContents());
+            $mtd = 0;
+            if($mtd_data->data && $mtd_data->data[0] && $mtd_data->data[0]->spend){
+                $mtd = $mtd_data->data[0]->spend;
+            }
+            $individual_acc_arr[$month_name_arr[$i]] = (float)$mtd;
+            
+         }
+
+         //get YTD data
+         $ytd_url = 'https://graph.facebook.com/v15.0/act_'.$row.'/insights?date_preset=this_year&access_token='.$access_token;
+         $ytd_response =Http::get($ytd_url);
+         $ytd_data = json_decode($ytd_response->getBody()->getContents());
+         $ytd = 0;
+         if($ytd_data->data && $ytd_data->data[0] && $ytd_data->data[0]->spend){
+            $ytd = $ytd_data->data[0]->spend;
+         }
+         $individual_acc_arr['YTD_Cost'] = (float)$ytd;
+         
           // get MTD sales from hyros
           $fb_hyros = [
               ['name' => 'OT_curlyshiny_Httpool','account_id'=>'868571924163034', 'email'=>'hello@curlyshiny.com', 'api_key'=>'68a862a6514d4abcb75799d2d127abbb4953333f5b961b35556db220aa103149'],
@@ -66,15 +106,14 @@ class FbApiController extends Controller
                           'API-Key' => $fb_hyros_row['api_key'], 
                       ])->get('https://api.hyros.com/v1/api/v1.0/attribution', [
                           "attributionModel" => 'last_click',
-                          "startDate" => '2022-12-01',
-                          "endDate" => '2022-12-19',
+                          "startDate" => $this_year.'-'.$this_month.'-01',
+                          "endDate" => $this_year.'-'.$this_month.'-'.$today,
                           'currency' => 'user_currency',
                           "level" => 'facebook_adset',
                           "fields" => 'revenue, sales, total_revenue',
                           "ids" => $as_set_row->id,
                           "dayOfAttribution" => false,
                       ]);
-                      // dd($get_hyros_data);
                       if($get_hyros_data->getStatusCode() == 200){
                           $data = json_decode($get_hyros_data->getBody()->getContents());
                           // dd($data->result[0]->revenue);
