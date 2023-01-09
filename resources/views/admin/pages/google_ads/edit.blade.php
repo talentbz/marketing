d5e8b448fab047058a3f34a8484a460bea9e1857
rockstar <leragnezdina@mail.ru>
2023-01-09 13:27:06 +0300

    change modal name

--- resources/views/admin/pages/google_ads/edit.blade.php
+++ resources/views/admin/pages/google_ads/edit.blade.php
@@ -2,7 +2,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title mt-0" id="updateModalLabel">Update Shopify</h5>
+                <h5 class="modal-title mt-0" id="updateModalLabel">Update Hyros</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal"
                     aria-label="Close"></button>
             </div>
